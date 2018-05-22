75491445f4a01e42f894dae4ff939af9a76bab59
Nick Fox-Gieg <nick@fox-gieg.com>
2018-05-22 15:36:47 -0400

    slideshow test

---
+++ Assets/Scripts/Slideshow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slideshow : MonoBehaviour {
+
+    public Texture2D[] pics;
+    public float picDelay = 1f;
+    public Renderer ren;
+    public int picCounter = 0;
+    public bool runSlideshow = true;
+
+    private Material mat;
+
+	private void Start() {
+        mat = ren.sharedMaterial;
+        mat.mainTexture = pics[picCounter];
+        if (runSlideshow) startSlideshow();
+	}
+	
+    private IEnumerator picAdvance() {
+        while (runSlideshow) {
+            yield return new WaitForSeconds(picDelay);
+            picForward();
+        }
+    }
+
+    public void startSlideshow() {
+        runSlideshow = true;
+        StartCoroutine(picAdvance());
+    }
+
+    public void stopSlideshow() {
+        runSlideshow = false;
+    }
+
+    public void picForward() {
+        picCounter++;
+        if (picCounter > pics.Length - 1) picCounter = 0;
+        mat.mainTexture = pics[picCounter];
+    }
+
+    public void picBack() {
+        picCounter--;
+        if (picCounter < 0) picCounter = pics.Length - 1;
+        mat.mainTexture = pics[picCounter];
+    }
+
+}
